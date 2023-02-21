9936c369e7da5c235205af045bc724ee26adb355
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-21 13:39:10 -0500

    complete some labs

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == 'admin' or username) == 'ADMIN ' and password == 12345:
+        return "Access Granted"
+    else:
+        return 'Access denied'
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return 'Brisk!'
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
     # your code here
@@ -15,3 +24,6 @@ def fizzbuzz(num):
 def calculator(operation, num1, num2):
     # your code here
     pass
+
+
+

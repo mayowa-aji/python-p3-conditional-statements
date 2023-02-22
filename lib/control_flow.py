652d3d2e93c430d2ed6a11f127ccb36f01aa1df7
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-22 13:48:13 -0500

    finish lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    if (username == 'admin' or username) == 'ADMIN ' and password == 12345:
-        return "Access Granted"
+    if (username == 'admin' or username == 'ADMIN') and password == "12345":
+        return "Access granted"
     else:
         return 'Access denied'
 
+admin_login("admin", "2334444")
+
 def hows_the_weather(temperature):
     if temperature < 40:
-        return 'Brisk!'
+        return "It's brisk out there!"
     elif temperature >= 40 and temperature <= 65:
         return "It's a little chilly out there!"
     elif temperature > 85:
@@ -16,14 +18,31 @@ def hows_the_weather(temperature):
     else:
         return "It's perfect out there!"
 
-
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
+
+print(fizzbuzz(3))
+
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == "/":
+        return num1/num2
+    else:
+        print("Invalid operation!")
 
 
 
